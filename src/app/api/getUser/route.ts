661ae3bac25ec8a9eec7617772f0a9141e45996c
zarif007 <zarifhuq786@gmail.com>
2023-08-12 19:51:00 +0600

    [FIXED]: user api

--- src/app/api/getUser/route.ts
+++ src/app/api/getUser/route.ts
@@ -2,20 +2,18 @@ import { prisma } from "@/lib/db";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
+const handler = async (req: Request, res: Response) => {
+  const { searchParams } = new URL(req.url || "");
+  const userId = searchParams.get("userId") || "";
 
-const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { searchParams } = new URL(req.url || "");
-    const userId = searchParams.get("userId") || '';
+  const user = await prisma.user.findFirst({
+    where: {
+      externalId: userId,
+    },
+  });
+  if (!user) return NextResponse.json({}, { status: 500 });
 
-    const user = await prisma.user.findFirst({
-        where: {
-            externalId: userId,
-        },
-    })
-    if (!user)
-      return res.statusCode = 500;
-
-    return NextResponse.json({ user: user.attributes })
+  return NextResponse.json({ user: user.attributes });
 };
 
-export const GET = handler
+export const GET = handler;
