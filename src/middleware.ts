01e118dcfc778e3d397f3686765264afbc8ebed2
zarif007 <zarifhuq786@gmail.com>
2023-08-12 19:56:59 +0600

    [FIXED]: user api

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/user"]
+  publicRoutes: [
+    "/",
+    "/api/webhooks/user",
+    "/api/getUser",
+    "/api/generateBanner",
+  ],
 });
 
 export const config = {
