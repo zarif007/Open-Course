7be417437f26dcfea7791d86b359cf977ea8b471
zarif007 <zarifhuq786@gmail.com>
2023-09-04 20:01:03 +0600

    [MADE]: course page lazy lodded

--- src/app/(courses)/courses/page.tsx
+++ src/app/(courses)/courses/page.tsx
@@ -1,17 +1,70 @@
+"use client";
+
 import CourseCard from "@/components/Course.Card";
 import { ICourse } from "@/types/course";
 import { v1MainEndpoint } from "@/utils/apiEndpoints";
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
+import CourseSkeleton from "@/components/Skeletons/Course.Skeleton";
 
-const Courses = async () => {
-  const { data } = await axios.get(`${v1MainEndpoint}/course`);
-  const courses = data.data;
+const LIMIT = 3;
+
+const getCourses = async (page: number) => {
+  const { data } = await axios.get(
+    `${v1MainEndpoint}/course?page=${page}&limit=${LIMIT}`
+  );
+  return data.data;
+};
+
+const Courses = () => {
+  const { ref, inView } = useInView();
+
+  const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      ["courses"],
+      async ({ pageParam = 1 }) => await getCourses(pageParam),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          const nextPage =
+            lastPage.length === LIMIT ? allPages.length + 1 : undefined;
+          return nextPage;
+        },
+      }
+    );
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+
+  const courses = data?.pages.flatMap((page) => page);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-      {courses.map((course: ICourse) => {
-        return <CourseCard key={course.id} course={course} />;
-      })}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-12">
+      {isFetchingNextPage ? (
+        <React.Fragment>
+          {new Array(LIMIT).fill(0).map((_, index) => (
+            <div key={index} className="px-3 pb-3">
+              <CourseSkeleton />
+            </div>
+          ))}
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          {courses?.map((course: ICourse, index: number) => {
+            return courses.length === index + 1 ? (
+              <div ref={ref} key={course.id}>
+                <CourseCard course={course} />
+              </div>
+            ) : (
+              <CourseCard key={course.id} course={course} />
+            );
+          })}
+        </React.Fragment>
+      )}
     </div>
   );
 };
