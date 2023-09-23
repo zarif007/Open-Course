ca1e2da79e2545479f105f66ee610dbf4946515e
zarif007 <zarifhuq786@gmail.com>
2023-09-23 21:46:56 +0600

    [MADE]: course and enrollstate same endpoint

--- src/components/ui/EmojiPicker.Dialog.tsx
+++ src/components/ui/EmojiPicker.Dialog.tsx
@@ -3,9 +3,12 @@
 import React from "react";
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
+import { Theme } from "emoji-picker-react";
+
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/Dialog";
 import { FaFaceGrinBeam } from "react-icons/fa6";
 import { useTheme } from "next-themes";
+import EmojiPicker from "emoji-picker-react";
 
 const EmojiPickerDialog = ({
   handleAddEmoji,
@@ -14,15 +17,17 @@ const EmojiPickerDialog = ({
 }) => {
   const { theme } = useTheme();
   const handleEmojiOnSelect = (emoji: any) => {
-    handleAddEmoji(emoji.native);
+    handleAddEmoji(emoji.emoji);
   };
+
+  const emojiTheme = theme === "dark" ? Theme.DARK : Theme.LIGHT;
   return (
     <Dialog>
       <DialogTrigger>
         <FaFaceGrinBeam className={`h-5 w-5 cursor-pointer`} />
       </DialogTrigger>
       <DialogContent className="p-0 max-w-fit">
-        <Picker theme={theme} data={data} onEmojiSelect={handleEmojiOnSelect} />
+        <EmojiPicker theme={emojiTheme} onEmojiClick={handleEmojiOnSelect} />
       </DialogContent>
     </Dialog>
   );
