e646afe19d96176eeed336f2dd05dc7358cf2b90
zarif007 <zarifhuq786@gmail.com>
2023-11-25 15:59:36 +0600

    [CHANGED]: course topic schema

--- src/components/course-doc/Doc.Display.tsx
+++ src/components/course-doc/Doc.Display.tsx
@@ -6,7 +6,7 @@ const DocDisplay = ({ courseTopic }: { courseTopic: ICourseTopic }) => {
   const version = courseTopic.versions.length - 1;
   const content = courseTopic.versions[version].data as IDocContent;
   return (
-    <div className="rounded p-3 mx-auto w-[100%] h-[45vh] md:h-[80vh] border-[3px] border-rose-500">
+    <div className="rounded p-3 mx-auto w-[100%] min-h-[45vh] md:min-h-[80vh] border-[3px] border-rose-500">
       <div
         dangerouslySetInnerHTML={{
           __html: DOMPurify.sanitize(content.content),
